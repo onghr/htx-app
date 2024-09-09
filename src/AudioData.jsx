129620328628b8a7507f5b5ad4e4b4f019b4032b
Hui Rong <nitez119@gmail.com>
2024-09-10 00:11:45 +0800

    tidy up codes

--- src/AudioData.jsx
+++ src/AudioData.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector, useDispatch } from 'react-redux';
+import { deleteAudio } from "./audioSlice";
 // import { Link } from 'react-router-dom';
 
 export default function AudioData(props) {
@@ -9,40 +10,40 @@ export default function AudioData(props) {
   const dispatch = useDispatch();
   const [isDelete, setIsDelete] = React.useState(false);
 
+  function AudioItem({ id, title, description, categories, uploadDate, fileSize }) {
+    return (
+      <>
+        <tr key={id}>
+          <td>{title}</td>
+          <td>{description}</td>
+          <td>
+            <div className="badge-cont">
+              {categories}
+            </div>
+          </td>
+          <td>{uploadDate}</td>
+          <td>{fileSize}</td>
+          <td className="audio-delete">
+            <button className="btn btn-primary btn-lg btn-danger"><i className="bi bi-trash3-fill"></i> Delete</button>
+          </td>
+        </tr>
+      </>
+    );
+  }
+
+
   return (
-    <>
-    {
       audios.map(
-        (audio, index) => {
+        (audio) => {
           const categories = (audio.category).split(',');
           const catItems = categories.map((cat) =>
-            <span class="badge bg-success">{cat}</span>
+            <span className="badge bg-success">{cat}</span>
           );
           return (
-            <tr id={index}>
-              <td>{audio.title}</td>
-              <td>{audio.description}</td>
-              <td>
-                <div className="badge-cont">
-                  {catItems}
-                </div>
-              </td>
-              <td>{audio.uploadDate}</td>
-              <td>{audio.fileSize}</td>
-              <td>{audio.fileFormat}</td>
-              <td className="audio-delete">
-                <button onClick={
-                  () => {
-                    setIsDelete(!isDelete)
-                  }
-                } className="btn btn-primary btn-lg btn-danger"><i className="bi bi-trash3-fill"></i> Delete</button>
-              </td>
-            </tr>
+            <AudioItem id={audio.id} title={audio.title} description={audio.description} categories={catItems} uploadDate={audio.uploadDate} fileSize={audio.fileSize} />
           )
         }
       )
-    }
-    </>
   );
 
 /*
@@ -56,7 +57,7 @@ export default function AudioData(props) {
 
     return (
       (!audioItem.deleted && audioItem.show) && (
-        <tr class={className} id={index}>
+        <tr className={className} id={index}>
           <td>{audioItem.title}</td>
           <td>{audioItem.desc}</td>
           <td>
